245e53e22c14e46536f4aa715e5bc3d253e70908
Paulo Santana <paulo.santana.r@gmail.com>
2021-02-08 11:31:27 +0000

    feat(filter/more): implement grayscale function

---
+++ pset4/more/filter/helpers.c
@@ -0,0 +1,41 @@
+#include <math.h>
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE blue = image[i][j].rgbtBlue;
+            BYTE green = image[i][j].rgbtGreen;
+            BYTE red = image[i][j].rgbtRed;
+
+            BYTE average = round((blue + green + red) / 3.0);
+
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
