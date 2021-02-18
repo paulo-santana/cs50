5d3db54bc73747bee8ac3d9557c8ecaf0f3858c4
Paulo Santana <paulo.santana.r@gmail.com>
2021-02-18 15:22:34 +0000

    feat(recover): quit if problems are found

---
+++ pset4/recover/recover.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./recover file-name.raw\n");
+        return 1;
+    }
+
+    char *filename = argv[1];
+
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL)
+    {
+        printf("Could not open the file %s\n", filename);
+        return 1;
+    }
+}
